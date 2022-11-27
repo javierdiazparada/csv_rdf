1e71293fa357f3ffc6ff225a997c674423f0fc0c
javierdiazparada <xavierdiazpa@gmail.com>
2022-11-27 20:48:02 -0300

    Third Update

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ url = 'http://raw.githubusercontent.com/javierdiazparada/csv_rdf/main/Games.csv'
 df = pd.read_csv(url, sep=",", quotechar='"')
 
 name = URIRef('name')
+sales = URIRef('sales')
 director = URIRef('director')
 genre = URIRef('genre')
 videoGame = URIRef('VideoGame')
@@ -14,12 +15,12 @@ releaseDate = URIRef('releaseDate')
 developer = URIRef('doap#developer')
 publisher = URIRef('publisher')
 
-
 g = Graph()
 ex = Namespace('http://example.org/')
 schema = Namespace('http://schema.org/')
 useful = Namespace('http://usefulinc.com/ns/')
 purl = Namespace('http://purl.org/dc/elements/1.1/')
+
 
 def name_default(sold):
     snew = sold
@@ -34,16 +35,23 @@ def name_default(sold):
     snew = snew.replace('$', '')
     return snew
 
-for index, row in df.iterrows():
-    givenName = name_default(row['Name'])
-    g.add((URIRef(ex+givenName), RDF.type, URIRef(schema+videoGame)))
-    g.add((URIRef(ex+givenName), URIRef(schema+name), Literal(row['Name'], datatype=XSD.string)))
-    g.add((URIRef(ex+givenName), URIRef(schema+genre), Literal(row['Genre'], datatype=XSD.string)))
-    g.add((URIRef(ex+givenName), URIRef(schema+videoGameSeries), Literal(row['Series'], datatype=XSD.string)))
-    g.add((URIRef(ex+givenName), URIRef(schema+releaseDate), Literal(row['Release'], datatype=XSD.gMonthDay)))
-    g.add((URIRef(ex+givenName), URIRef(useful+developer), Literal(row['Developer'], datatype=XSD.string)))
-    g.add((URIRef(ex+givenName), URIRef(purl+publisher), Literal(row['Publisher'], datatype=XSD.string)))
 
-with open("RDF_Games.txt",'x', encoding="utf-8") as f:
+for index, row in df.iterrows():
+
+    givenName = name_default(row['Name'])
+    g.add((URIRef(ex + givenName), RDF.type, URIRef(schema + videoGame)))
+    g.add((URIRef(ex + givenName), URIRef(schema + name), Literal(row['Name'], datatype=XSD.string)))
+    g.add((URIRef(ex + givenName), URIRef(ex + sales), Literal(row['Sales'] * 1000000.0, datatype=XSD.float)))
+    Genres = row['Genre'].split(',\xa0')
+    for i in Genres:
+        j = i.capitalize()
+        g.add((URIRef(ex + givenName), URIRef(schema + genre), Literal(j)))
+
+    g.add((URIRef(ex + givenName), URIRef(schema + videoGameSeries), Literal(row['Series'])))
+    g.add((URIRef(ex + givenName), URIRef(schema + releaseDate), Literal(row['Release'], datatype=XSD.gMonthDay)))
+    g.add((URIRef(ex + givenName), URIRef(useful + developer), Literal(row['Developer'])))
+    g.add((URIRef(ex + givenName), URIRef(purl + publisher), Literal(row['Publisher'])))
+
+with open("RDF_Games.txt", 'x', encoding="utf-8") as f:
     text = g.serialize(format='turtle').encode('UTF-8').decode()
     f.write(text)
